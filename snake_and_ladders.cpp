71a1e0941da1af2bf66cae99918a52fad2c7dff4
BrahmaTeja <80442374+DBrahmaTeja@users.noreply.github.com>
2023-01-24 22:17:54 +0530

    Code Added

--- snake_and_ladders.cpp
+++ snake_and_ladders.cpp
@@ -100,3 +100,22 @@ int main()
 		<< getMinDiceThrows(moves, N);
 	return 0;
 }
+
+//OR
+vector<int> dist(n * n + 1, -1);
+        queue<int> q;
+        dist[1] = 0;
+        q.push(1);
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for (int next = curr + 1; next <= min(curr + 6, n * n); next++) {
+                auto [row, column] = cells[next];
+                int destination = board[row][column] != -1 ? board[row][column] : next;
+                if (dist[destination] == -1) {
+                    dist[destination] = dist[curr] + 1;
+                    q.push(destination);
+                }
+            }
+        }
+        return dist[n * n];
